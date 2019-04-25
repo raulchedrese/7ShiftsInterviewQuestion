15384cebefdefcb010a161618f657badffca704b
Mohammad Ali Yektaie <yektaie112358@gmail.com>
2019-04-25 11:52:25 -0400

    Added implementation + tests of the second question.

---
+++ src/com/seven_shifts/calculators/Part2IgnoreNewLineCalculator.java
@@ -0,0 +1,34 @@
+package com.seven_shifts.calculators;
+
+import com.seven_shifts.StringCalculator;
+
+/**
+ * Implementation to support second question. Specification:
+ *
+ * Change the Add method to handle new lines in the input format
+ *    a. Example: “1\n,2,3” - Result: “6”
+ *    b. Example 2: “1,\n2,4” - Result: “7”
+ */
+public class Part2IgnoreNewLineCalculator implements StringCalculator {
+    @Override
+    public int add(String text) {
+        // The only difference between this part and the previous one is to ignore the new lines, the
+        // easiest way is to just remove them and proceed like part one.
+        text = text.replace("\n", "");
+
+        // Note that I could use the method from Part1SimplestCalculator.add(), but the question wants
+        // to change the code incrementally.
+        int result = 0;
+
+        if (!text.equals("")) {
+            String[] parts = text.split(",");
+
+            for (String part : parts) {
+                result += Integer.valueOf(part);
+            }
+        }
+
+        return result;
+
+    }
+}
---
+++ src/test/seven_shifts/Part2IgnoreNewLineCalculatorTest.java
@@ -0,0 +1,74 @@
+package test.seven_shifts;
+
+import com.seven_shifts.StringCalculator;
+import com.seven_shifts.calculators.Part2IgnoreNewLineCalculator;
+import org.junit.Test;
+
+import java.util.Hashtable;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+public class Part2IgnoreNewLineCalculatorTest {
+    @Test
+    public void testEmptyStringResult() {
+        StringCalculator calculator = new Part2IgnoreNewLineCalculator();
+        int result = calculator.add("");
+
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void testNewLineOnlyStringResult() {
+        StringCalculator calculator = new Part2IgnoreNewLineCalculator();
+        int result = calculator.add("\n");
+
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void testCorrectStringResult() {
+        // Note that this could also be done with parameterized test, but let's keep things simple.
+        Hashtable<String, Integer> examples = getSetOfExamplesWithResult();
+
+        StringCalculator calculator = new Part2IgnoreNewLineCalculator();
+
+        for (String text : examples.keySet()) {
+            int expected = examples.get(text);
+            int actual = calculator.add(text);
+
+            assertEquals(expected, actual);
+        }
+    }
+
+    private Hashtable<String, Integer> getSetOfExamplesWithResult() {
+        Hashtable<String, Integer> examples = new Hashtable<>();
+        examples.put("\n1\n", 1); // Test single number with single digit, leading and trailing new lines
+        examples.put("12\n\n", 12); // Test single number with multiple digits
+        examples.put("12\n,26\n\n", 38); // Test 2 numbers
+        examples.put("10\n,\n24\n\n,\n60", 94); // Test 3 numbers
+        examples.put("1\n,2,3", 6); // Test the example in specification
+        examples.put("1,\n2,4", 7); // Test the example in specification
+
+        return examples;
+    }
+
+    @Test
+    public void testIncorrectStringResult() {
+        // Note that this could also be done with parameterized test, but let's keep things simple.
+        // Making sure of the code returning wrong result is as important as making sure of the correct
+        // results
+
+        Hashtable<String, Integer> examples = getSetOfExamplesWithResult();
+
+        StringCalculator calculator = new Part2IgnoreNewLineCalculator();
+
+        for (String text : examples.keySet()) {
+            int wrongAnswer = examples.get(text) + 1; // +1 to make sure it is not the correct answer
+            int actual = calculator.add(text);
+
+            assertNotEquals(wrongAnswer, actual);
+        }
+
+    }
+}
